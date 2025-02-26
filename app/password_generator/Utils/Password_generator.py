102c30e7af67c731cc2670c7c1b5ba50057c6dd6
Foool1 <julek.pryczkowski@wp.pl>
2025-02-26 22:23:40 +0100

    apfas

--- app/password_generator/Utils/Password_generator.py
+++ app/password_generator/Utils/Password_generator.py
@@ -4,7 +4,8 @@ def generate_password(length):
     chars = 'abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPRSTUVWXYZ!@£$%^&*()?01234356789'
 
     password = ''
-
+    if length<4: return 'password to short'
+    elif length>32: return 'password to long'
     for y in range(length):
         password += random.choice(chars)
     return password
